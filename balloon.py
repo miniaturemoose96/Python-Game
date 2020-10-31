e59ad15389d725dadc7d2441b49281eb8a9d271b
Alejandro Bernal <alexbernal32396@gmail.com>
2020-10-31 02:53:11 -0700

    Added the balloon bounds, bg image and enemy flyer.

--- balloon.py
+++ balloon.py
@@ -1,12 +1,10 @@
 import pygame
 import sys
 
-# background color
-background_color = (0, 0, 255)
 # Set the width and the height of the window
 (width, height) = (300, 300)
 # Sprite width
-char_width = 23
+char_width = 20
 # set the screen fro pygame to the width and height tuple defined above
 # set the name of the pygame project at the top of the window
 screen = pygame.display.set_mode((width, height))
@@ -14,7 +12,10 @@ pygame.display.set_caption('Sky Balloons')
 # game time
 clock = pygame.time.Clock()
 # Load my char image
-character = pygame.image.load(r'/Users/txt-17/PycharmProjects/PyGame/sky_balloons/sprites/kirby.png')
+character = pygame.image.load(r'/Users/txt-17/PycharmProjects/PyGame/sky_balloons/sprites/balloon.png')
+# Load the enemy players
+obstacle = pygame.image.load(r'/Users/txt-17/PycharmProjects/PyGame/sky_balloons/sprites/spike_flyer.png')
+background = pygame.image.load(r'/Users/txt-17/PycharmProjects/PyGame/sky_balloons/sprites/sky.png')
 
 
 # This function will display my image
@@ -22,11 +23,18 @@ def my_char(x, y):
     screen.blit(character, (x, y))
 
 
+# this function creates enemies
+def create_enemies(x, y):
+    screen.blit(obstacle, (x, y))
+
+
 # Game Loop
 def game_loop():
     # Set image position
     img_x = 20
     img_y = 30
+    enemy_x = 100
+    enemy_y = 88
     # Movement
     char_pos_x = 0
     char_pos_y = 0
@@ -40,13 +48,13 @@ def game_loop():
         # Movement - this changes the position of the
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                char_pos_x = -2
+                char_pos_x = -4
             elif event.key == pygame.K_RIGHT:
-                char_pos_x = 2
+                char_pos_x = 4
             elif event.key == pygame.K_UP:
-                char_pos_y = -2
+                char_pos_y = -4
             elif event.key == pygame.K_DOWN:
-                char_pos_y = 2
+                char_pos_y = 4
 
         # Checks if the keys have been released
         # if this logic is not added then we cannot
@@ -59,12 +67,12 @@ def game_loop():
         img_x += char_pos_x
         img_y += char_pos_y
 
-        # Fill the screen with the background color
-        screen.fill(background_color)
+        # Add sprites
+        screen.blit(background, [0, 0])
         my_char(img_x, img_y)
+        create_enemies(enemy_x, enemy_y)
 
         # bounding to the window
-        # X coordinate
         if img_x > width - char_width:
             print('right bounce')
             img_x -= 5
@@ -77,14 +85,6 @@ def game_loop():
         if img_y < 0:
             print('top bounce')
             img_y = 0
-
-
-
-        # Bounding to the window
-        # Y coordinate
-        # if img_y > height - char_width or img_y < 0:
-        #     print('Y Position the player hit the wall')
-        #     img_y = 0
 
         # flip will allow us to display the graphics and allow us to see the change
         pygame.display.flip()
